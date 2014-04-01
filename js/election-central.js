27a01274680a34ee27ac664eed7408d14d33a295
Richard Escobedo <escobedorichard@gmail.com>
2014-04-01 12:47:54 -0500

    Closed #26

--- js/election-central.js
+++ js/election-central.js
@@ -45,7 +45,7 @@ function initialize() {
               $('<div></div>').addClass('panel-body').appendTo(panel);
               
               //Wrap panels in a div that allows it to function inside the Bootstrap grid
-              $(panel).wrap('<div class="col-lg-4 col-md-4 grid-item"></div>');
+              $(panel).wrap('<div class="col-lg-4 col-md-4 item"></div>');
               
               var name = rows[i][0];
               var $namevar = $('<h3 class="panel-title"></h3>');
@@ -60,11 +60,15 @@ function initialize() {
               
               var committee = rows[i][1];
               var $committeevar = $('<div><span></span></div>');
-              $committeevar.text("Office seeking: " + committee).appendTo(panel + " .panel-body");
+              $committeevar.text(committee).appendTo(panel + " .panel-body");
+              var $committeelabel = $('<strong></strong>');
+              $committeelabel.text("Office seeking: ").prependTo($committeevar);
               
               var classification = rows[i][2];
               var $classvar = $('<div><span></span></div>');
-              $classvar.text("Classification: " + classification).appendTo(panel + " .panel-body");
+              $classvar.text(classification).appendTo(panel + " .panel-body");
+              var $classlabel = $('<strong></strong>');
+              $classlabel.text("Classification: ").prependTo($classvar);
               
               var legislation = rows[i][3];
               var $legislationvar = $("<div><span></span></div>");
